0c6e9f9b5eedb2e5c4cc8845102df3bf94706656
Sunil Pachlangia <sunil.pachlangia@avadolearning.com>
2019-11-08 18:47:47 +0530

    Add UserPreferences Model

---
+++ classes/Entities/UserPreferences.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Avado\MoodleAbstractionLibrary\Entities;
+
+/**
+ * Class UserPreferences
+ *
+ * @package local\dashboard\Entities
+ */
+class UserPreferences extends BaseModel
+{
+    /**
+     * @var string
+     */
+    protected $table = 'user_preferences';
+}
