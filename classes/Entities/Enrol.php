1106185b18f08a08f0aa631101b1c106f52a2fbd
Vijay Savakhande <vijay.savakhande@avadolearning.com>
2019-11-13 16:15:31 +0530

    Added relationship to get course

--- classes/Entities/Enrol.php
+++ classes/Entities/Enrol.php
@@ -2,12 +2,32 @@
 
 namespace Avado\MoodleAbstractionLibrary\Entities;
 
+/**
+ * Class Enrol
+ * @package Avado\MoodleAbstractionLibrary\Entities
+ */
 class Enrol extends BaseModel
 {
+    /**
+     * @var string
+     */
     protected $table = 'enrol';
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function role()
     {
         return $this->belongsTo(Role::class, 'roleid', 'id');
+    }
+
+    /**
+     * Get the all course based on enrollment
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function course()
+    {
+        return $this->belongsTo(Course::class,'courseid','id');
     }
 }
