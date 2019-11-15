5a1433d61c356eaae622d55ba464118ec918b063
Jonathan Shad <jonathan.shad@avadolearning.com>
2019-11-15 11:04:36 +0000

    Add CourseCompletions Model

---
+++ classes/Entities/CourseCompletions.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Avado\MoodleAbstractionLibrary\Entities;
+
+/**
+ * Class CourseCompletions
+ * @package Avado\MoodleAbstractionLibrary\Entities
+ */
+class CourseCompletions extends BaseModel
+{
+    /**
+     * @var string
+     */
+    protected $table = 'course_completions';
+}
