20d5b435b40ae4ae67ff510224ffdda96ee76225
Vijay Savakhande <vijay.savakhande@avadolearning.com>
2019-11-18 16:53:36 +0530

    Added core entity

---
+++ classes/Entities/MessageRead.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Avado\MoodleAbstractionLibrary\Entities;
+
+/**
+ * Class MessageRead
+ * @package Avado\MoodleAbstractionLibrary\Entities
+ */
+class MessageRead extends BaseModel
+{
+    protected $table = 'message_read';
+}
