8ba7b0f7ad88d8d8d9bf125a91a099f951052efa
Vijay Savakhande <vijay.savakhande@avadolearning.com>
2019-11-20 14:26:31 +0530

    Removed custom changes from core entity

--- classes/Entities/UserLastaccess.php
+++ classes/Entities/UserLastaccess.php
@@ -2,8 +2,6 @@
 
 namespace Avado\MoodleAbstractionLibrary\Entities;
 
-use moodle_url;
-
 /**
  * Class UserLastaccess
  * @package Avado\MoodleAbstractionLibrary\Entities
@@ -14,37 +12,6 @@ class UserLastaccess extends BaseModel
      * @var string
      */
     protected $table = 'user_lastaccess';
-
-    /**
-     * @var array
-     */
-    protected $appends = ['course_url'];
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function course()
-    {
-        return $this->belongsTo(Course::class, 'courseid', 'id');
-    }
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function user()
-    {
-        return $this->belongsTo(User::class, 'userid', 'id');
-    }
-
-    /**
-     * @return moodle_url
-     * @throws \moodle_exception
-     */
-    public function getCourseUrlAttribute()
-    {
-        $courseUrl = new moodle_url('/course/view.php', ['id' => $this->courseid]);
-        return $courseUrl->raw_out();
-    }
 }
 
 
