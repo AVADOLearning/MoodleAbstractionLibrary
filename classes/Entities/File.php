c545ab4515c893f1e4087837c9e9513afbc6df69
Vijay Savakhande <vijay.savakhande@avadolearning.com>
2020-02-26 16:39:40 +0530

    Add File entity

---
+++ classes/Entities/File.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Avado\MoodleAbstractionLibrary\Entities;
+
+/**
+ * Class File
+ * @package Avado\MoodleAbstractionLibrary\Entities
+ */
+class File extends BaseModel
+{
+    /**
+     * @var string
+     */
+    protected $table = 'files';
+}
