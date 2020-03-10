b460f71c47fd939766f7680b4639d28ace2abed9
nateg101 <natelliottgreen34@gmail.com>
2020-03-10 14:46:18 +0000

    feat[classes/Entities]: Add CourseSection class.

---
+++ classes/Entities/CourseSection.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Avado\MoodleAbstractionLibrary\Entities;
+
+/**
+ * Class CourseSection
+ * @package Avado\MoodleAbstractionLibrary\Entities
+ */
+class CourseSection extends BaseModel
+{
+    protected $table = "course_sections";
+}
